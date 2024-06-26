4cba8474ec965fe2b015ec84ba7710bdbf21ba64
Robin TOURNÉ <robin.tourne@ynov.com>
2024-06-26 11:43:02 +0200

    GREEN: custom separators error handling

--- src/katas/Kata2.ts
+++ src/katas/Kata2.ts
@@ -1,4 +1,5 @@
 export const findCustomSeparator = (input: string): string => {
+	if (!input.startsWith('//')) return '';
 	const endOfCustomSeparator = input.indexOf('\n');
 	const customSeparator = input.slice(2, endOfCustomSeparator);
 	return customSeparator;
@@ -7,13 +8,16 @@ export const findCustomSeparator = (input: string): string => {
 export const add = (input: string): number => {
 	if (input === '') return 0;
 	const customSeparator = findCustomSeparator(input);
+	console.debug({customSeparator});
 	let strNumbers = input;
-	if (customSeparator) strNumbers = input.slice(input.indexOf('\n') + 1);
+	if (customSeparator) {
+		strNumbers = input.slice(input.indexOf('\n') + 1)
+		if (strNumbers.includes(',')) throw new Error(`'${customSeparator}' expected but ',' found at position ${strNumbers.indexOf(',')}.`);
+	};
 
 	const defaultSeparators = /,|\n/; // matches comma (,) or newline (\n)
 	if (strNumbers.endsWith(',') || strNumbers.endsWith('\n')) throw new Error('Invalid input');
 
 	const numbers = strNumbers.split(customSeparator || defaultSeparators).map(Number);
-	console.debug(numbers);
 	return numbers.reduce((acc, num) => acc + num);
 }
