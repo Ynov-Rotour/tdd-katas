c86f6b9b0b547c351ed1da55fda40ebcc5806676
Robin TOURNÉ <robin.tourne@ynov.com>
2024-06-26 11:10:23 +0200

    REFACTOR: numbers separated by commas

--- src/katas/Kata2.ts
+++ src/katas/Kata2.ts
@@ -1,8 +1,5 @@
 export const add = (input: string): number => {
 	if (input === '') return 0;
-	const commaIndex = input.indexOf(',');
-	if (commaIndex === -1) return parseInt(input);
-	const firstNumber = input.substring(0, commaIndex);
-	const secondNumber = input.substring(commaIndex + 1);
-	return parseInt(firstNumber) + parseInt(secondNumber);
+	const numbers = input.split(',').map(Number);
+	return numbers.reduce((acc, num) => acc + num);
 }
