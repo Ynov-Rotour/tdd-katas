f048f9c0e9ca31270eacd33c4a25d0ed31a98834
Robin TOURNÉ <robin.tourne@ynov.com>
2024-06-26 11:19:13 +0200

    GREEN: Throw if separator at the end

--- src/katas/Kata2.ts
+++ src/katas/Kata2.ts
@@ -1,6 +1,7 @@
 export const add = (input: string): number => {
 	if (input === '') return 0;
 	const regex = /,|\n/; // matches comma (,) or newline (\n)
+	if (input.endsWith(',') || input.endsWith('\n')) throw new Error('Invalid input');
 	const numbers = input.split(regex).map(Number);
 	console.debug(numbers);
 	return numbers.reduce((acc, num) => acc + num);
