c52a830be7a46f7f3b6fdcdbef1126b398ad4c4c
Robin TOURNÉ <robin.tourne@ynov.com>
2024-06-26 11:32:11 +0200

    GREEN: Custom separators usage

--- src/katas/Kata2.ts
+++ src/katas/Kata2.ts
@@ -6,9 +6,13 @@ export const findCustomSeparator = (input: string): string => {
 
 export const add = (input: string): number => {
 	if (input === '') return 0;
+	const customSeparator = findCustomSeparator(input);
+	if (customSeparator) {
+		input = input.slice(input.indexOf('\n') + 1);
+	}
 	const regex = /,|\n/; // matches comma (,) or newline (\n)
 	if (input.endsWith(',') || input.endsWith('\n')) throw new Error('Invalid input');
-	const numbers = input.split(regex).map(Number);
+	const numbers = input.split(customSeparator || regex).map(Number);
 	console.debug(numbers);
 	return numbers.reduce((acc, num) => acc + num);
 }
